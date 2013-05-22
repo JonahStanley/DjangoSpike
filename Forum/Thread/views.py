ed22f95b63e2f7857168626b6b3354fb8051fc18
Jonah Stanley <Jonah_Stanley@brown.edu>
2013-05-22 13:37:55 -0400

    Set Up Views

--- Forum/Thread/views.py
+++ Forum/Thread/views.py
@@ -1,1 +1,18 @@
 # Create your views here.
+from django.http import HttpResponse
+
+
+def hello(request):
+    return HttpResponse("hi")
+
+
+def login(request, in_or_out):
+    return HttpResponse("TESTING")
+
+
+def register(request):
+    return HttpResponse("TESTING2")
+
+
+def edit_profile(request):
+    return HttpResponse("TESTING3")
