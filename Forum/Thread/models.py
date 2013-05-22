83fdcdfdcd6894aa3740ccd69c42d3d1ddbc85a6
Ian Hoover <irh@Ians-MacBook-Air.local>
2013-05-22 14:22:57 -0400

    added post class and database

--- Forum/Thread/models.py
+++ Forum/Thread/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+	userid = models.IntegerField()
+	text = models.TextField()
+
+
+# class User(models.Model):
+# 	username = models.CharField(max_length = 30, unique = True)
+# 	password = models.CharField(max_length = 30)
+# 	first_name = models.CharField(max_length = 30)
+# 	last_name = models.CharField(max_length = 30)
