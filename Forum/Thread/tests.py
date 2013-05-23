1f77a2a7f15d7f9baf8b534afd9c4b5848cd8763
Jonah Stanley <Jonah_Stanley@brown.edu>
2013-05-23 10:51:25 -0400

    First test

--- Forum/Thread/tests.py
+++ Forum/Thread/tests.py
@@ -4,13 +4,13 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
 from django.test import TestCase
+from Thread.models import Post
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ModelTest(TestCase):
+    def test_create_post(self):
+        p = Post.objects.create(userid=1, text="HI")
+        p.save()
+        self.assertEqual(p.userid, Post.objects.get(userid=1).userid)
+        self.assertEqual(p.text, Post.objects.get(userid=1).text)
